bd8a5d0a3461fdc4762d3cf2e4e095ecb7fdc1ff
Viktor Churikov <scanlave@gmail.com>
2021-08-23 03:17:28 +0800

    Appearance

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -3,15 +3,17 @@
 //  ColorizedApp
 //
 //  Created by Виктор Чуриков on 23.08.2021.
-//
+//  Приношу свои искренние извенения за кривой и страшный код. 
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorDisplay: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
